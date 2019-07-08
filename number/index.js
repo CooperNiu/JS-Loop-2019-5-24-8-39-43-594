59b43fc7256ffe1eb5e55ddf3078a7b7854d4719
CooperNiu <niuyanno.1@outlook.com>
2019-07-08 18:56:27 +0800

    Create index.js

---
+++ number/index.js
@@ -0,0 +1,13 @@
+    
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+function outputNumberParity(beginNumber, endNumber) {
+  for (let i = beginNumber; i <= endNumber; i++) {
+    document.write(i + (isEven(i) ? "是偶数。</br>" : "是奇数。</br>"));
+  }
+}
+const beginNumber = 1;
+const endNumber = 20;
+outputNumberParity(beginNumber, endNumber);
